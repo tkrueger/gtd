54476ed8402b8282df944fbebf03afceed209eff
Thorsten Krüger <thorstenkg@gmail.com>
2013-06-30 15:58:40 +0200

    require every file in infrastructure module

--- backend/app/backend/infrastructure/infrastructure.rb
+++ backend/app/backend/infrastructure/infrastructure.rb
@@ -1,7 +1,5 @@
 module GTD
   module EVENTS
-    require_relative 'event_stream'
-    require_relative 'event_store'
-    require_relative 'file_backend'
+    Dir[ File.dirname(__FILE__)+"/*.rb"].each {|file| require file }
   end
 end
