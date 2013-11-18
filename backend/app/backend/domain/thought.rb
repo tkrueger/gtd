43a8fac57a759f50d527e2f4bbe39d73777dc24a
Thorsten Krüger <thorstenkg@gmail.com>
2013-11-18 21:47:38 +0100

    convert thoughts to actions

--- backend/app/backend/domain/thought.rb
+++ backend/app/backend/domain/thought.rb
@@ -2,10 +2,18 @@ require_relative "../../../../backend/app/backend/infrastructure/aggregate"
 require_relative "../infrastructure/messaging"
 
 ThoughtCaptured = Class.new(GTD::Messaging::Message)
+ConvertedToAction = Class.new(GTD::Messaging::Message)
+
 
 class Thought < Aggregate
 
   attr_reader :text
+
+  def self.capture(id, text)
+    thought = Thought.new(id)
+    thought.captured(text)
+    thought
+  end
 
   def captured(text)
     apply(ThoughtCaptured.new(text: text))
@@ -15,4 +23,11 @@ class Thought < Aggregate
     @text = msg.text
   end
 
+  def convert_to_action(action_id)
+    apply(ConvertedToAction.new(action_id: action_id))
+  end
+
+  def on_converted_to_action(msg)
+
+  end
 end
--- backend/spec/backend/domain/thought_spec.rb
+++ backend/spec/backend/domain/thought_spec.rb
@@ -6,9 +6,16 @@ require_relative "../../../../backend/app/backend/infrastructure/infrastructure"
 describe Thought do
 
   it 'should have a text' do
-    thought = Thought.new(123)
-    thought.captured "This is an example thought."
+    thought = Thought.capture 123, "This is an example thought."
+    thought.id.should == 123
     thought.text.should == "This is an example thought."
+    thought.should have(1).unsaved_events
+  end
+
+  it 'can be converted into an action' do
+    thought = Thought.capture 123, "This is an example thought."
+    thought.clear_unsaved_events
+    action = thought.convert_to_action 124
     thought.should have(1).unsaved_events
   end
 end
