84c400f1423258e355c5fcd014216ff32d18b1c6
Thorsten Krüger <thorstenkg@gmail.com>
2013-11-18 21:47:13 +0100

    allow for message validations

--- backend/app/backend/infrastructure/messaging.rb
+++ backend/app/backend/infrastructure/messaging.rb
@@ -3,7 +3,63 @@ require 'ostruct'
 
 module GTD
   module Messaging
+
+    class PresenceValidator
+
+      def initialize(attribute)
+        @attribute = attribute
+      end
+
+      def validate(instance)
+        unless instance.respond_to? @attribute.to_sym
+          instance.errors.add(@attribute, :must_be_present)
+        end
+      end
+    end
+
+    class Errors
+
+      class Error
+        attr_accessor :attribute, :reason
+
+        def initialize(attribute, reason)
+          @attribute = attribute
+          @reason = reason
+        end
+
+        def to_s
+          "#{attribute}: #{reason}"
+        end
+      end
+
+      def initialize
+        @errors = []
+      end
+
+      def add(attribute, reason)
+        @errors << Error.new(attribute, reason)
+      end
+
+      def empty?
+        @errors.empty?
+      end
+
+      def combined_messages
+        @errors.join ", "
+      end
+    end
+
     class Message < OpenStruct
+
+      @@validators = []
+
+      attr_reader :errors
+
+      def initialize(hash=nil)
+        super(hash)
+        throw "there was an error: #{errors.combined_messages}" unless valid?
+      end
+
       def to_json(*)
         as_json.to_json
       end
@@ -20,6 +76,23 @@ module GTD
         new(object['data'] || object[:data])
       end
 
+      def validations
+        # default: noop
+      end
+
+      def validates_presence_of(var)
+        validate_with(PresenceValidator, var)
+      end
+
+      def validate_with(validator, attribute)
+        validator.new(attribute).validate self
+      end
+
+      def valid?
+        @errors = Errors.new
+        validations
+        errors.empty?
+      end
     end
   end
 end
--- backend/spec/backend/infrastructure/message_spec.rb
+++ backend/spec/backend/infrastructure/message_spec.rb
@@ -6,14 +6,25 @@ describe 'Messages' do
 
   it 'can be declared' do
     TestMessage1  = Class.new(GTD::Messaging::Message)
-    TestMessage1.new({}).should be_a Message
+    TestMessage1.new({}).should be_a GTD::Messaging::Message
   end
 
-  it 'can add creation checks' do
+  it 'can add creation checks manually' do
     TestMessage2  = Class.new(GTD::Messaging::Message) do
       def initialize(hash=nil)
         super(hash)
         throw "field xyz is mandatory" if xyz.nil?
+      end
+    end
+
+    expect { TestMessage2.new(foo: "bar") }.to raise_error
+    expect { TestMessage2.new(xyz: "foo") }.not_to raise_error
+  end
+
+  it 'can add creation checks declaratively' do
+    TestMessage2  = Class.new(GTD::Messaging::Message) do
+      def validations
+        validates_presence_of :xyz
       end
     end
 
